ff3c497ff4ac4ba6d02706e4f90d0cecc3953a24
tosaki <wzx1459931670@outlook.com>
2024-07-19 17:52:54 +0800

    add photo lib converter

---
+++ photo_converter.py
@@ -0,0 +1,143 @@
+from video_converter import in_format as video_extensions, setup_logging, process_directory
+import logging
+import traceback
+import subprocess
+import datetime
+from concurrent.futures import ThreadPoolExecutor
+from pathlib import Path
+from tqdm import tqdm
+import argparse
+
+parser = argparse.ArgumentParser(description='Convert images to AVIF format')
+parser.add_argument('source_dir', type=str, help='Source directory')
+parser.add_argument('target_dir', type=str, help='Target directory')
+parser.add_argument('--crf', type=int, default=36, help='CRF value')
+parser.add_argument('--max_resolution', type=int,
+                    default=3024 * 4032, help='Max resolution in pixels')
+parser.add_argument('--max_workers', type=int, default=8,
+                    help='Max number of workers')
+# parser.add_argument('--log_file', type=str,
+#                     default='convert_images.log', help='Log file')
+parser.add_argument("--video_ffmpeg_args", type=str, help="Additional arguments to pass to ffmpeg.",
+                    default="-loglevel error -stats -c:v libsvtav1 -preset 4 -crf 28 -pix_fmt yuv420p10le -c:a libopus -b:a 64k")
+args = parser.parse_args()
+
+# # Set up logging
+# logging.basicConfig(filename=args.log_file, level=logging.ERROR,
+#                     format='%(asctime)s %(levelname)s %(name)s %(message)s')
+setup_logging()
+
+image_extensions = ('*.png', '*.jpg', '*.jpeg', '*.webp',
+                    '*.heic', '*.heif', '*.gif', '*.tiff', '*.tif')
+
+
+def process_image(args):
+    filepath, source_dir, target_dir, crf, max_resolution = args
+    relative_path = filepath.relative_to(source_dir)
+    target_path = target_dir / relative_path
+    target_path = target_path.with_suffix('.avif')
+
+    try:
+        target_path.parent.mkdir(parents=True, exist_ok=True)
+
+        temp_png_created = False  # Flag to track if a temp PNG file is created
+
+        if filepath.suffix.lower() in ['.heic', '.heif']:
+            # Convert HEIC or HEIF to PNG using ImageMagick
+            png_filepath = filepath.with_suffix('.png')
+            subprocess.run(['magick', str(filepath), '-compress', 'lossless',
+                           str(png_filepath)], check=True, stdout=subprocess.DEVNULL)
+
+            filepath = png_filepath  # Use the PNG file for the rest of the process
+            temp_png_created = True  # Set flag
+
+        # Check image resolution using ffprobe
+        ffprobe_process = subprocess.run(['ffprobe', '-v', 'error', '-select_streams', 'v:0', '-show_entries',
+                                         'stream=width,height', '-of', 'csv=s=x:p=0', str(filepath)], capture_output=True, text=True)
+        width, height = map(int, ffprobe_process.stdout.strip().split('x'))
+        resolution = width * height
+
+        cmd = ["ffmpeg", "-i", str(filepath)]
+
+        if resolution > max_resolution:
+            # If the resolution is greater than max_resolution, scale it down
+            # Calculate target resolution
+            target_width = round(width * (max_resolution / resolution) ** 0.5)
+            target_height = round(
+                height * (max_resolution / resolution) ** 0.5)
+            cmd.extend(["-vf", f"scale={target_width}:{target_height}"])
+        else:
+            # Otherwise, convert the image as is
+            pass
+
+        cmd.extend(["-c:v", "libsvtav1", "-crf", str(crf), "-still-picture", "1",
+                   str(target_path), "-cpu-used", "0", "-y", "-hide_banner", "-loglevel", "error"])
+        subprocess.run(cmd, stdout=subprocess.DEVNULL, check=True)
+
+        # Check if the image has DateTimeOriginal exif data using exiftool
+        exiftool_process = subprocess.run(
+            ['exiftool', '-DateTimeOriginal', str(filepath), '-m'], capture_output=True, text=True)
+
+        exif_output = exiftool_process.stdout
+
+        if 'DateTimeOriginal' not in exif_output:
+            # If there is no DateTimeOriginal data, set it to the file's modification time
+            mod_time = datetime.datetime.fromtimestamp(
+                filepath.stat().st_mtime)
+            mod_time_str = mod_time.strftime('%Y:%m:%d %H:%M:%S')
+            subprocess.run(['exiftool', '-DateTimeOriginal=' + mod_time_str,
+                           str(target_path), '-m'], check=True, stdout=subprocess.DEVNULL)
+
+        # Copy all other exif data
+        subprocess.run(['exiftool', '-TagsFromFile', str(filepath), '-all:all',
+                       str(target_path), '-m'], check=True, stdout=subprocess.DEVNULL)
+
+        # Remove "_original" backup file created by exiftool
+        backup_file = target_path.with_name(target_path.name + '_original')
+        if backup_file.exists():
+            backup_file.unlink()
+        if temp_png_created:
+            filepath.unlink()
+    except Exception:
+        logging.error(f"Error processing image {
+                      filepath}: {traceback.format_exc()}")
+
+
+def convert_images(source_dir, target_dir, crf, max_resolution, max_workers=1):
+    source_dir = Path(source_dir)
+    target_dir = Path(target_dir)
+
+    # image_files = []
+    # for image_extension in image_extensions:
+    #     image_files.extend(list(source_dir.glob('**/' + image_extension)))
+
+    image_files = [f for f in source_dir.rglob('*') if f.suffix.lower() in image_extensions and '@' not in str(f)]
+
+    # remove redundant .MOV live photos
+    live_photos = [f for f in source_dir.rglob('*') if f.suffix.lower() == '.mov' and f.with_suffix('.HEIC') in image_files or f.with_suffix('.heic') in image_files]
+
+    # Convert images
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        list(tqdm(executor.map(process_image, [(image_file, source_dir, target_dir, crf, max_resolution)
+             for image_file in image_files]), total=len(image_files), ncols=80))
+
+    # Convert videos
+    process_directory(source_dir, target_dir, delete_original=False, ffmpeg_args=args.video_ffmpeg_args)
+
+    # remove redundant .MOV live photos in target_dir
+    for live_photo in live_photos:
+        target_live_photo = target_dir / live_photo.relative_to(source_dir)
+        target_live_photo = target_live_photo.with_suffix('.mkv')
+        if target_live_photo.exists():
+            target_live_photo.unlink()
+
+
+
+if __name__ == '__main__':
+    source_dir = args.source_dir
+    target_dir = args.target_dir
+    crf = args.crf
+    max_resolution = args.max_resolution
+
+    convert_images(source_dir, target_dir, crf, max_resolution,
+                   max_workers=args.max_workers)
--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 export SVT_LOG=1
 # nice -n 15 python video-converter.py
-nice -n 15 python video-converter-new.py /mnt/synology/inpersistent/convert/2406/input/ /mnt/synology/inpersistent/convert/2406/output/ --delete
+rm -r ~/temp_ffmpeg/*
+nice -n 15 python video_converter.py /mnt/synology/inpersistent/convert/2406/input/ /mnt/synology/inpersistent/convert/2406/output/ --delete
